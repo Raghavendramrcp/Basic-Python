34a07d0851431d72ad2709ea77e8cac38066d845
Raghavendramrcp <phaniraghavendra.ksv@gmail.com>
2022-11-09 00:13:14 +0530

    CPR class has been updated in oops.py

--- Basic Python Scripts/oops.py
+++ Basic Python Scripts/oops.py
@@ -1,16 +1,25 @@
-class Family:
+class CPR():
 
-    def __init__(self, name, age, height, weight):
-        self.name = name
-        self.age = age
-        self.height = height
-        self.weight = weight
+    def __init__(self, high,low, close):
+        self.high = high
+        self.low = low
+        self.close = close
 
-    def father(self):
-        print(self.name)
-        print(self.age)
-        print(self.height)
-        print(self.weight)
+    def central_pivot(self):
+        central_pivot = (float(self.high)+float(self.low)+float(self.close))/3
+        print(central_pivot)
+        return central_pivot
+    def top_central_pivot(self):
+        top_central_pivot = (float(self.high)+float(self.low))/2
+        print(top_central_pivot)
+        return abs(top_central_pivot)
+    def bottom_central_pivot(self):
+        pivot = (float(self.high)+float(self.low)+float(self.close))/3
+        top_pivot = (float(self.high)+float(self.low))/2
+        bottom_pivot = pivot+(pivot-top_pivot)
+        print(bottom_pivot)
+        return abs(bottom_pivot)
 
-f = Family("Ramana", 55, 160, 60)
-f.father()
+bn1 = CPR(41516,41051,41258)
+
+print(bn1.central_pivot(),bn1.top_central_pivot(),bn1.bottom_central_pivot())
